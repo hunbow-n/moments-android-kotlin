3f448fc544e7979b3e46743703cdbc3bc8242fe6
Никита Сержевский <hunbow.n@gmail.com>
2024-10-15 02:31:16 +0300

    initial, nav bar

---
+++ app/src/main/java/com/hunbow/momentbooks/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.hunbow.momentbooks
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.replace
+import com.hunbow.momentbooks.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.bottNav)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        replaceFragment(Layouts())
+
+        binding.bottNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.layouts_icon -> replaceFragment(Layouts())
+                R.id.drafts_icon -> replaceFragment(Drafts())
+                R.id.promotions_icon -> replaceFragment(Promotions())
+                R.id.cart_icon -> replaceFragment(Cart())
+                R.id.profile_icon -> replaceFragment(Profile())
+                else -> {
+
+                }
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
+    }
+}
